dceea75db2ab3260faa52196f99d7935f6b4593d
elead1 <ericleadbetter@gmail.com>
2020-08-15 10:02:49 -0400

    Fkn asyncio

--- missionmanifest.py
+++ missionmanifest.py
@@ -7,9 +7,11 @@ import re
 import datetime
 import sqlite3
 from time import sleep
-from threading import Lock
-from typing import List
+from threading import Thread, Lock
+import asyncio
+from typing import List, Tuple, Dict
 import pickle
+import atexit
 
 
 datetime_fmt = re.compile(r"(\d{4})-((?:0\d)|(?:1(?:0|1|2)))-((?:0\d)|(?:1\d)|(?:2\d)|(?:3(?:0|1))) ((?:0\d)|(?:1\d)|(?:2(?:0|1|2|3))):([0-5]\d)")
@@ -24,12 +26,13 @@ ERRORS = [
 
 bot = commands.Bot(command_prefix="!manifest ")
 datastore_file = os.environ.get("MISSIONMANIFEST_DB") or "missionmanifest.db"
-data_store_conn = None
 data_store_lock = None
 initialize = False
+poll_thread = None
+run_threads = True
 
 
-def validDate(date_str: str):
+def vali_date(date_str: str):
     date_match = datetime_fmt.fullmatch(date_str)
     if not date_match:
         return None, 0
@@ -55,149 +58,182 @@ async def emojis(ctx):
     await ctx.send("Server's emojis: {}".format(" ".join(emoji_list)))
 
 
-def get_available_emojis(server: int) -> List[str]:
-    assert isinstance(data_store_conn, sqlite3.Connection)
+@bot.command()
+async def deconstruct(ctx: commands.Context, message_url: str):
+    msg = await ctx.fetch_message(int(message_url.split("/")[-1]))
+    assert isinstance(msg, discord.Message)
+    embed = msg.embeds[0] if len(msg.embeds) > 0 else None
+    if not embed:
+        await ctx.send("Linked message contains no embeds.")
+        return
+    embed_dict = embed.to_dict()
+    response = "***Key***: *Value*\n"
+    for key, val in embed_dict.items():
+        response += "**{}**: {}\n".format(key, val)
+    await ctx.send(response)
+
+
+def get_available_emojis(server: int) -> List[Tuple[int, str]]:
     used = []
-    with data_store_conn.cursor() as curs:
+    data_store_lock.acquire()
+    with sqlite3.connect(datastore_file) as data_store_conn:
+        curs = data_store_conn.cursor()
         for row in curs.execute("SELECT emoji FROM Emoji WHERE server=?;", (server,)):
             used.append(pickle.loads(row[0]))
+        curs.close()
+    data_store_lock.release()
     used = set(used)
     # Get all emojis
-    all_emojis = set(bot.get_guild(server).emojis)
+    all_emojis = set((e.id, e.name) for e in bot.get_guild(server).emojis)
     return list(all_emojis - used)
 
 
-def create_mission_embed(channel_id, dm_name, description, emoji, levels, mission_name, when):
+def create_mission_embed(channel_id: int, dm_name: str, description: str, emoji: Tuple[int, str], levels: str,
+                         mission_name: str, when: str,
+                         roster_size: int = None, responses: List[discord.Message] = None):
     embed = Embed(title="Manifest for \"{}\"".format(mission_name))
     embed.add_field(name="Tier", value=levels, inline=False)
     embed.add_field(name="DM", value=dm_name, inline=False)
     embed.add_field(name="Date", value=when, inline=False)
     embed.add_field(name="Description", value=description, inline=False)
     embed.add_field(name="RSVP In", value="<#{}>".format(channel_id), inline=False)
-    embed.add_field(name="Signup Emoji", value=emoji, inline=False)
+    embed.add_field(name="Signup Emoji", value=":{}:".format(emoji[1]), inline=False)
     embed.set_thumbnail(url=bot.user.avatar_url)
     embed.set_footer(text="Last Scanned: {}".format(str(datetime.datetime.utcnow())))
+    if roster_size and responses:
+        embed.add_field(name="Total Signups", value=str(roster_size), inline=False)
+        roster = ""
+        for response in responses:
+            roster += "* [{}]({})\n".format(response.author.nick or response.author.name, response.jump_url)
+        embed.add_field(name="Roster", value=roster, inline=False)
     return embed
 
 
-def poll_thread():
-    assert isinstance(data_store_conn, sqlite3.Connection)
+def embed_to_friendly_dict(embed: discord.Embed) -> Dict[str, object]:
+    embed_dict = embed.to_dict()
+    embed_fields = embed_dict["fields"]  # List[Dict[str, object]] where object is usually str
+    embed_fields = {el['name']: el['value'] for el in embed_fields}
+    embed_fields["Title"] = embed_dict["Title"]
+    # Parse channel_id field
+    embed_fields["RSVP In"] = int(re.match(r"<#(\d)>", embed_fields["RSVP In"]).groups()[0])
+    return embed_fields
+
+
+def scan_history(oldest: int, scan_location: Tuple[int, int], scan_targets: Dict[Tuple[int, str], discord.Message]):
+    server = bot.get_guild(scan_location[0])
+    assert isinstance(server, discord.Guild)
+    scan_channel = server.get_channel(scan_location[1])
+    scan_limit = datetime.datetime.utcfromtimestamp(oldest)
+    mission_emojis = set(scan_targets.keys())
+    mission_updates = {tgt: None for tgt in scan_targets.values()}
+    messages = asyncio.run(scan_channel.history(after=scan_limit).flatten())
+    for message in messages:
+        if len(message.reactions) == 1:  # Restrict to messages with a single emoji
+            # get a list of emojis used to react to the message by the message's author (react.me)
+            emoji_reacts = [(react.emoji[discord.Emoji].id, react.emoji[discord.Emoji].name)
+                            for react in message.reactions if react.me]
+            # find any mission emojis in the react list for this message
+            emojis_on_msg = mission_emojis.intersection(set(emoji_reacts))
+            assert len(emojis_on_msg) == 1
+            tracker_msg = scan_targets[emojis_on_msg[0]]  # This is the mission tracking msg object
+            if mission_updates[tracker_msg]:
+                tmp = mission_updates[tracker_msg]
+                new = (tmp[0] + 1, tmp[1] + [message])
+                mission_updates[tracker_msg] = new
+            else:
+                mission_updates[tracker_msg] = (1, [message])  # how many responses found, list of responses
+    for tracker_msg, responses in mission_updates.items():
+        assert isinstance(tracker_msg, discord.Message)
+        tracker_msg_obj = server.get_channel(tracker_msg[1]).fetch_message(tracker_msg[0])
+        old_embed_dict = embed_to_friendly_dict(tracker_msg_obj.embeds[0])
+        new_embed = create_mission_embed(old_embed_dict["RSVP In"], old_embed_dict["DM"], old_embed_dict["Description"],
+                                         old_embed_dict["Signup Emoji"], old_embed_dict["Tier"], old_embed_dict["Title"],
+                                         old_embed_dict["Date"], responses[0], responses[1])
+        tracker_msg_obj.edit(embed=new_embed)
+
+
+def poll_thread_body():
     to_remove = []
     to_scan = {}
     oldest_per_channel = {}  # ?? maybe? for tracking oldest post on a per channel basis
-    with data_store_conn.cursor() as curs:
+    data_store_lock.acquire()
+    with sqlite3.connect(datastore_file) as data_store_conn:
+        curs = data_store_conn.cursor()
         for row in curs.execute("SELECT Mission.*, Emoji.emoji FROM Mission, Emoji "
                                 "WHERE Mission.emojiId=Emoji.emojiId;"):
             when = int(row[4])
             if when <= datetime.datetime.now(datetime.timezone.utc).timestamp():
                 # Mission already started or is in the past, clean it up.
-                to_remove.append((int(row[0]), int(row[6])))
+                to_remove.append((int(row[0]), int(row[6])))  # Item 1: missionId, Item 2: emojiId
                 continue
             # Mission still active
             scan_location = (int(row[1]), int(row[2]))  # Item 1: server_id, Item 2: scan_channel_id
-            scan_target = (row[5], row[7])  # Item 1: discord.Message blob, Item 2: emoji blob
-
             if scan_location in to_scan:
-                to_scan[scan_location].append(scan_target)
+                # Key: Tuple[int, str] representing discord. Emoji, Value: Tuple[int, int] representing message id, channel id
+                to_scan[scan_location][pickle.loads(row[7])] = pickle.loads(row[5])
             else:
-                to_scan[scan_location] = [scan_target]
+                to_scan[scan_location] = {pickle.loads(row[7]): pickle.loads(row[5])}
             mission_time = int(row[3])
             if scan_location in oldest_per_channel:
                 if oldest_per_channel[scan_location] < mission_time:
                     continue
             oldest_per_channel[scan_location] = mission_time
-    ''' BIG BRAIN: collect relevant info from Db first, then iterate through server.scan_channels and
-    find all replies at once instead of re-scanning.'''
-    # Iterate through scan_channel's history between mission_create_time and present to look for emoji
-    for scan_location, scan_target in to_scan.items():
-        server = bot.get_guild(scan_location[0])
-        assert isinstance(server, discord.Guild)
-        scan_channel = server.get_channel(scan_location[1])
-        scan_limit = datetime.datetime.utcfromtimestamp(oldest_per_channel[scan_location])
-        for message in await scan_channel.history(after=scan_limit):
-            if scan_target[1] in message.reactions:  # Need to get actual emoji entity (id:name)
-                pass
-        tracking_msg = pickle.loads(scan_target[0])
-        tracking_embed = tracking_msg.embeds[0]
-    # delete anything in the to_remove list
-    pass
+        # Iterate through scan_channel's history between mission_create_time and present to look for emoji
+        for scan_location, scan_targets in to_scan.items():
+            t = Thread(target=scan_history, args=(oldest_per_channel[scan_location], scan_location, scan_targets))
+            t.start()
+        # delete anything in the to_remove list
+        for el in to_remove:
+            curs.execute("DELETE FROM Emoji WHERE emojiId=?", (el[1],))
+            curs.execute("DELETE FROM Mission WHERE missionId=?", (el[0],))
+        curs.close()
+        data_store_conn.commit()
+    data_store_lock.release()
+
+
+def poll_thread_loop(frequency: float = 0.0033):  # default to once every 5 minutes
+    while run_threads:
+        poll_thread_body()
+        sleep(1 / frequency)
 
 
 @bot.event
 async def on_ready():
     global data_store_lock
-    global data_store_conn
     global initialize
+    global poll_thread
     if not data_store_lock:
         data_store_lock = Lock()
-    if not data_store_conn:
+    initialize = not os.path.exists(datastore_file)
+    if initialize:
         data_store_lock.acquire()
-        initialize = not os.path.exists(datastore_file)
-        data_store_conn = sqlite3.connect(datastore_file)
-        if initialize:
-            assert isinstance(data_store_conn, sqlite3.Connection)
+        with sqlite3.connect(datastore_file) as data_store_conn:
             curs = data_store_conn.cursor()
             curs.execute("CREATE TABLE Emoji (emojiId INTEGER PRIMARY KEY, server INTEGER, emoji BLOB);")
             curs.execute("CREATE TABLE Mission "
                          "(missionId INTEGER PRIMARY KEY, serverId INTEGER, scanChannelId INTEGER, "
                          "missionCreateTime INTEGER, missionTime INTEGER,"
-                         "tracking_msg BLOB, emojiId INTEGER);")
+                         "trackingMsg BLOB, emojiId INTEGER);")
             data_store_conn.commit()
             initialize = False
         data_store_lock.release()
     print("MissionManifest has entered chat; id: {0}".format(bot.user))
+    poll_thread = Thread(target=poll_thread_loop)
+    poll_thread.start()
 
 
 @bot.event
 async def on_disconnect():
-    global data_store_conn
-    if data_store_conn:
-        assert isinstance(data_store_lock, Lock)
-        data_store_lock.acquire()
-        assert isinstance(data_store_conn, sqlite3.Connection)
-        data_store_conn.close()
-        data_store_conn = None
-        data_store_lock.release()
+    global run_threads
+    run_threads = False
+    poll_thread.join()
     print("MissionManifest has exited.")
 
 
 @bot.command(description="<FILL ME IN>")
-async def scan(ctx: commands.Context, mission_name: str, channel_id: str):
-    global last_scan
-    channel_id = channel_id_fmt.match(channel_id).group(1)
-    channel = await bot.fetch_channel(channel_id)
-    now = datetime.datetime.utcnow()
-    if not last_scan:
-        last_scan = now - datetime.timedelta(days=14)
-    num_signups = 0  # This will end up pulled from the cache eventually
-    responses = []
-    # query message history
-    async for message in channel.history(limit=None, before=now, after=last_scan, oldest_first=True):
-        lines = message.content.split('\n')
-        try:
-            assert len(lines) > 1
-        except AssertionError:
-            # Skip any single-line messages; sign-up messages must have the mission name as the first line.
-            continue
-        if mission_name.lower() in lines[0].lower():
-            # found the mission we're looking for
-            if player_role in message.author.roles:
-                num_signups += 1
-                # The format assumes line 0 = mission name, line 1 = character name, line i...n = extra info
-                character = lines[1]
-                message_url = r"https://discordapp.com/channels/{}/{}/{}".format(ctx.guild.id, ctx.channel.id, message.id)
-                responses.append((message.author.nick or message.author.name, character, message_url))
-    # Build the response Embed
-    embed = Embed(title="Manifest for \"{}\"".format(mission_name))
-    embed.add_field(name="Total Signups", value=str(num_signups), inline=False)
-    roster = ""
-    for response in responses:
-        roster += "* {}: {}\n".format(response[0], "[{}]({})".format(response[1], response[2]))
-    embed.add_field(name="Roster", value=roster, inline=False)
-
-    await ctx.send(embed=embed)
-    # Record the current scan as the most recent for caching purposes.
-    last_scan = now
+async def scan(ctx: commands.Context):
+    print("called scan!")
+    poll_thread_body()
 
 
 @bot.command(description="<WHAT IS MY PURPOSE>")
@@ -215,38 +251,42 @@ async def track(ctx: commands.Context, mission_name: str, when: str, levels: str
         await ctx.send("Only users with the DM role can create missions.")
         return
     channel_id = channel_id_fmt.match(track_channel).group(1)
-    when_posix, err = validDate(when)
+    when_posix, err = vali_date(when)
     if not when_posix:
         await ctx.send(ERRORS[err])
         return
 
     emoji = random.choice(get_available_emojis(ctx.guild.id))
+    emoji_blob = pickle.dumps(emoji)
     description = " ".join(description)
 
     embed = create_mission_embed(channel_id, ctx.author.nick or ctx.author.name,
                                  description, emoji, levels, mission_name, when)
     tracking_msg = await ctx.send(embed=embed)
+    tracking_msg_url_parts = tracking_msg.jump_url.split("/")
+    # Item 1: message id, Item 2: channel id
+    tracking_msg_info = pickle.dumps((int(tracking_msg_url_parts[-1]), int(tracking_msg_url_parts[-2])))
 
-    assert isinstance(data_store_conn, sqlite3.Connection)
     # Create entry in db for new mission.
     data_store_lock.acquire()
-    emoji_blob = pickle.dumps(emoji)
-    data_store_conn.execute("INSERT INTO Emoji (server, emoji) VALUES (?, ?);", (ctx.guild.id, emoji_blob))
-    curs = data_store_conn.cursor()
-    curs.execute("SELECT emojiId from Emoji WHERE emojiId=(SELECT MAX(emojiId) FROM Emoji);")
-    emoji_id = curs.fetchone()[0]
-    curs.close()
-    tracking_msg_blob = pickle.dumps(tracking_msg)
-    data_store_conn.execute("INSERT INTO Mission (serverId, scanChannelId, "
-                            "missionCreateTime, missionTime, "
-                            "tracking_msg, emojiId) "
-                            "VALUES (?, ?, ?, ?, ?, ?);",
-                            (ctx.guild.id, channel_id,
-                             datetime.datetime.now(tz=datetime.timezone.utc).timestamp(), when_posix,
-                             tracking_msg_blob, emoji_id))
-    data_store_conn.commit()
+    with sqlite3.connect(datastore_file) as data_store_conn:
+        data_store_conn.execute("INSERT INTO Emoji (server, emoji) VALUES (?, ?);", (ctx.guild.id, emoji_blob))
+        curs = data_store_conn.cursor()
+        curs.execute("SELECT emojiId from Emoji WHERE emojiId=(SELECT MAX(emojiId) FROM Emoji);")
+        emoji_id = curs.fetchone()[0]
+        curs.close()
+
+        data_store_conn.execute("INSERT INTO Mission (serverId, scanChannelId, "
+                                "missionCreateTime, missionTime, "
+                                "trackingMsg, emojiId) "
+                                "VALUES (?, ?, ?, ?, ?, ?);",
+                                (ctx.guild.id, channel_id,
+                                 datetime.datetime.now(tz=datetime.timezone.utc).timestamp(), when_posix,
+                                 tracking_msg_info, emoji_id))
+        data_store_conn.commit()
     data_store_lock.release()
 
+atexit.register(lambda: on_disconnect())
 
 token = os.environ.get("MISSIONMANIFEST_SECRET")
 bot.run(token)
